63fe0dbae11fc3344897bdc2cf5c4a1f88dc89f1
exst3zz <annapavtso@gmail.com>
2024-08-09 00:48:57 +0300

    done

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
+import initialContacts from "./data/data.json";
+
+function App() {
+  const [contacts, setContacts] = useState(() => {
+    const contactsFromLSData = JSON.parse(
+      window.localStorage.getItem("contacts")
+    );
+    if (contactsFromLSData !== null && contactsFromLSData.contacts.length > 4) {
+      return contactsFromLSData.contacts;
+    }
+    return initialContacts;
+  });
+
+  const [searchValue, setSearchValue] = useState("");
+
+  const filteredContacts = contacts.filter((contact) => {
+    return contact.name.toLowerCase().includes(searchValue.toLowerCase());
+  });
+
+  const addContact = (newContact) => {
+    setContacts((prevContactList) => {
+      return [...prevContactList, newContact];
+    });
+  };
+
+  const deleteContacts = (contactId) => {
+    setContacts((prevContactList) =>
+      prevContactList.filter((contact) => contact.id !== contactId)
+    );
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem("contacts", JSON.stringify({ contacts }));
+  }, [contacts]);
+
+  return (
+    <div className="phonebookWrap">
+      <h1>Phonebook</h1>
+
+      <ContactForm addContact={addContact} />
+      <SearchBox
+        searchValue={searchValue}
+        handleSearchChange={setSearchValue}
+      />
+      <ContactList
+        contacts={filteredContacts}
+        deleteContacts={deleteContacts}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,22 @@
+import { useId } from "react";
+import css from "./SearchBox.module.css";
+const SearchBox = ({ searchValue, handleSearchChange }) => {
+  const searchInputId = useId();
+  return (
+    <div className={css.searchWrap}>
+      <label className={css.searchLabel} htmlFor={searchInputId}>
+        Find contacts by name
+      </label>
+      <input
+        className={css.searchInput}
+        type="text"
+        name="search"
+        id={searchInputId}
+        value={searchValue}
+        onChange={(e) => handleSearchChange(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
